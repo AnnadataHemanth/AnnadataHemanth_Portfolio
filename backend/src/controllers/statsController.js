eeb4f48cca6b54349550e4d8ac097ae40b2c4ccf
AnnadataHemanth <annadathahemanth578@gmail.com>
2026-08-22 15:12:10 +0530

    Add certificate count to stats

--- backend/src/controllers/statsController.js
+++ backend/src/controllers/statsController.js
@@ -1,19 +1,27 @@
 import Project from '../models/Projects.js'
 import Skill from '../models/Skills.js'
 import Message from '../models/Message.js'
+import Certificate from '../models/Certificates.js'
 
 export const getStats = async (req, res, next) => {
   try {
-    const [projects, skills, messages] = await Promise.all([
+    const [
+      projects,
+      skills,
+      messages,
+      certificates,
+    ] = await Promise.all([
       Project.countDocuments(),
       Skill.countDocuments(),
       Message.countDocuments(),
+      Certificate.countDocuments(),
     ])
 
     res.status(200).json({
       projects,
       skills,
       messages,
+      certificates,
     })
   } catch (error) {
     next(error)
